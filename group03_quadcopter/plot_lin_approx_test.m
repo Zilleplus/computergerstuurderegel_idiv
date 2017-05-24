288f4db4f881f8acfca8b02b6ab6be584eadd978
Willem Melis <zilleplus@gmail.com>
2017-05-24 23:00:21 +0200

    cleaned up step response

--- group03_quadcopter/plot_lin_approx_test.m
+++
@@ -1,9 +0,0 @@
-% close all
-fig=figure(1);clf;
-subplot(1,2,1);
-plot(lin_model_stepresponse);
-legend('x','y','z','phi','theta','gamma');
-subplot(1,2,2);
-plot(non_lin_model_stepresponse);
-
-saveas(fig,'./report/img/lin_approx/step_all.eps');
